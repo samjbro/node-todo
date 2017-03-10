ba7bfa2a8fd9a8220259b356d19a30171f87c765
samjbro <sjbroughton1@gmail.com>
2017-03-10 19:02:10 +0000

    adds blank todo to top of page, formats todos for blank/existing

--- public/app.js
+++ public/app.js
@@ -3,12 +3,23 @@ $(() => {
     $.each(todos, (id,description) => {
       renderTodo(id,description);
     });
+    renderTodo();
   });
 });
 
 function renderTodo(id,description) {
   let todo = $($("#todotemplate").html());
+  id ? formatExisting(todo, id, description) : formatBlank(todo);
+  $("#todos").prepend(todo);
+  todo.find(".description").focus();
+}
+
+function formatExisting(todo, id, description) {
   todo.attr("id", id);
   todo.find(".description").val(description);
-  $("#todos").prepend(todo);
+  todo.find(".description").attr("placeholder", "Update todo");
+}
+
+function formatBlank(todo) {
+  todo.find(".done").hide();
 }
